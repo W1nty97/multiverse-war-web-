84cdd836c9f6252074ac83b21f64fcc161959768
W1nty97 <vaishanik17@gmail.com>
2025-03-16 02:05:45 +0300

    16.03.2025

--- src/Main/filterPopup/rareFilter/rareFilter.jsx
+++ src/Main/filterPopup/rareFilter/rareFilter.jsx
@@ -1,22 +1,29 @@
 import "./rareFilter.css";
 
 export default function RareFilter() {
+  function check(event) {
+    if (event.currentTarget.className === "check-block active") {
+      event.currentTarget.classList.remove("active");
+    } else {
+      event.currentTarget.classList.add("active");
+    }
+  }
   return (
     <>
       <div className="popup-content-all">
-        <div className="check-block" id="cammon">
+        <div className="check-block active" id="cammon" onClick={check}>
           <p>обычная</p>
         </div>
-        <div className="check-block" id="rare">
+        <div className="check-block active" id="rare" onClick={check}>
           <p>редкая</p>
         </div>
-        <div className="check-block" id="epic">
+        <div className="check-block active" id="epic" onClick={check}>
           <p>эпическая</p>
         </div>
-        <div className="check-block" id="legend">
+        <div className="check-block active" id="legend" onClick={check}>
           <p>легендарная</p>
         </div>
-        <div className="check-block" id="mythic">
+        <div className="check-block active" id="mythic" onClick={check}>
           <p>мифическая</p>
         </div>
       </div>
